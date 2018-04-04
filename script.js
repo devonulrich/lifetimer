527924400614b91c28e1af3f699bbfa985248e39
Devon Ulrich <dpulrich@gmail.com>
2018-04-04 12:51:43 -0400

    Changed format of JS file
    The JS file now follows the design pattern I use in most of my
    Javascript scripts, where one or multiple top-level variables
    house both public and private varibles, functions, or objects. I
    just find this style of JS coding much easier to organize and keep
    readable than any other pattern I've tried.

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-window.onload = function() {
-    var canvas = document.getElementById("current");
-	var ctx = canvas.getContext("2d");
-	ctx.fillStyle = "#FFF";
-	ctx.fillRect(0, 0, 900, 672);
+var UI = (function() {
 
-	var side = document.getElementById("sideSection");
-	for(var i = 0; i < 70; i++) {
-		var box = document.createElement("div");
-		box.className = "sideBox";
-		//box.appendChild(document.createTextNode("Year " + i));
+    return {
+        populateSideDiv: function() {
+            var canvas = document.getElementById("current");
+            var ctx = canvas.getContext("2d");
+            ctx.fillStyle = "#FFF";
+            ctx.fillRect(0, 0, 900, 672);
 
-		var topBox = document.createElement("div");
-		topBox.className = "topMiniBox";
-		topBox.appendChild(document.createTextNode("Year " + i));
-		box.appendChild(topBox);
+            var side = document.getElementById("sideSection");
+            for(var i = 0; i < 70; i++) {
+                var box = document.createElement("div");
+                box.className = "sideBox";
+                //box.appendChild(document.createTextNode("Year " + i));
 
-		for(var j = 1; j < 365; j++) {
-			var minibox = document.createElement("div");
-			minibox.className = "miniBox";
-			minibox.appendChild(document.createTextNode("Day " + (j + 1)));
-			box.appendChild(minibox);
-		}
-		side.appendChild(box);
-	}
-}
+                var topBox = document.createElement("div");
+                topBox.className = "topMiniBox";
+                topBox.appendChild(document.createTextNode("Year " + i));
+                box.appendChild(topBox);
+
+                for(var j = 1; j < 365; j++) {
+                    var minibox = document.createElement("div");
+                    minibox.className = "miniBox";
+                    minibox.appendChild(document.createTextNode("Day " + (j + 1)));
+                    box.appendChild(minibox);
+                }
+                side.appendChild(box);
+            }
+        }
+    };
+})();
+
+window.onload = UI.populateSideDiv;
