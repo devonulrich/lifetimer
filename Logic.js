62840a634fdfb97cea73ea221b59ab21e3f9a4e9
Devon Ulrich <dpulrich@gmail.com>
2018-04-07 21:45:01 -0400

    Added new file w/ function for validating birthdays
    This new function is inside "Logic.js", which will handle all the
    internal logic and math for dates and times.

---
+++ Logic.js
@@ -0,0 +1,16 @@
+var Logic = (function() {
+
+    return {
+        validateBDay: function(month, day, year) {
+            //returns true if the given birthdate (int integers) is valid,
+            //and false if it is not
+            if(year < 1950) return false;
+
+            var timestamp = Date.parse(month + "/" + day + "/" + year);
+            if(isNaN(timestamp)) return false;
+            if(timestamp > Date.now()) return false;//the given date is in the future
+
+            return true;
+        }
+    };
+})();
