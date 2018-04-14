1bc031fe46bde2816e66ce8215cfdbd21fda70de
Devon Ulrich <dpulrich@gmail.com>
2018-04-14 14:31:23 -0400

    Changed minimum birth year to 1940
    This means that almost everyone that is less than 80 years old can
    use this webpage. However, in the future it would be a good idea
    to change this minimum year within the code itself, rather than
    relying on a static piece of code.

--- js/Logic.js
+++ js/Logic.js
@@ -4,7 +4,7 @@ var Logic = (function() {
         validateBDay: function(month, day, year) {
             //returns true if the given birthdate (int integers) is valid,
             //and false if it is not
-            if(year < 1950) return false;
+            if(year < 1940) return false;
 
             var timestamp = Date.parse(month + "/" + day + "/" + year);
             if(isNaN(timestamp)) return false;
